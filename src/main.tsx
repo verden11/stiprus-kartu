0787592bac36d769b28431c07ed4072d61fe2be7
verden11 <justinas.reigis@gmail.com>
2022-03-28 00:05:48 +0300

    chore: remove firestore

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './services/firebase';
 
 import { Providers } from './Providers';
 
