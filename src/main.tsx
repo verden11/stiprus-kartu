01aa3b700979a78f30459f7c38833a2f396cecd1
verden11 <justinas.reigis@gmail.com>
2022-03-31 21:45:15 +0300

    chore: upgrade to react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 import { Providers } from './Providers';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Providers>
       <App />
     </Providers>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
