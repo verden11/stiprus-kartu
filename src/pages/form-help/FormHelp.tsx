9575497147aee66e1419baa144a808116827dca4
verden11 <justinas.reigis@gmail.com>
2022-04-01 23:02:43 +0300

    chore: add some form fields

--- src/pages/form-help/FormHelp.tsx
+++ src/pages/form-help/FormHelp.tsx
@@ -1,71 +1,124 @@
 import { memo } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+enum Sex {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+}
+
+enum Nationality {
+  Ukrainian = 'Ukrainian',
+  Polish = 'Polish',
+  Russian = 'Russian',
+  Lithuanian = 'Lithuanian',
+  Other = 'Other',
+}
 
 type Inputs = {
-  firstName: string;
+  fullName: string;
   email: string;
-  lastName: string;
+  phoneNumber: string;
+  dob: Date;
+  addressInUA: string;
+  sex: Sex;
+  nationality: Nationality;
 };
 
 export const FormHelp = () => {
+  const { formatMessage } = useIntl();
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm<Inputs>();
+
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     alert(JSON.stringify(data));
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        placeholder="Bill"
-        {...register('firstName', {
-          required: 'this is a required',
-          minLength: {
-            value: 2,
-            message: 'Min length is 2',
-          },
-        })}
-      />
-      <br />
-      {errors.firstName && errors.firstName.message}
-      <br />
+    <div className="max-w-6xl mx-6 md:mx-14 pt-3">
+      <h1>
+        <FormattedMessage id="temporaryHome" />
+      </h1>
+      <p>
+        <FormattedMessage id="fillTheForm" />
+      </p>
+      <p>
+        <FormattedMessage id="PersonalInfo" />
+      </p>
 
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        placeholder="Luo"
-        {...register('lastName', {
-          required: 'this is required',
-          minLength: {
-            value: 2,
-            message: 'Min length is 2',
-          },
-        })}
-      />
-      <br />
-      {errors.lastName && errors.lastName.message}
-      <br />
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-1 flex-col py-6">
+        <label htmlFor="fullName">
+          <FormattedMessage id="form.fullName" />
+        </label>
+        <input
+          placeholder="John Smith"
+          {...register('fullName', {
+            required: formatMessage({ id: 'form.required' }),
+            minLength: {
+              value: 2,
+              message: 'Min length is 2',
+            },
+          })}
+        />
+        <br />
+        {errors.fullName?.message}
+        <br />
 
-      <label htmlFor="email">Email</label>
-      <input
-        placeholder="bluebill1049@hotmail.com"
-        type="text"
-        {...register('email', {
-          required: 'this is required',
-          pattern: {
-            value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-            message: 'Invalid email address',
-          },
-        })}
-      />
-      <br />
-      {errors.email && errors.email.message}
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+        <label htmlFor="email">
+          <FormattedMessage id="form.email" />
+        </label>
+        <input
+          placeholder="johnsmith@email.com"
+          type="email"
+          {...register('email', {
+            required: 'this is required',
+            pattern: {
+              value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: 'Invalid email address',
+            },
+          })}
+        />
+        <br />
+        {errors.email?.message}
+        <br />
+
+        <label htmlFor="phoneNumber">
+          <FormattedMessage id="form.phone" />
+        </label>
+        <input
+          placeholder="+370XXXXXXXX"
+          {...register('phoneNumber', {
+            required: formatMessage({ id: 'form.required' }),
+            minLength: {
+              value: 2,
+              message: 'Min length is 2',
+            },
+          })}
+        />
+        <br />
+        {errors.phoneNumber?.message}
+        <br />
+
+        <label htmlFor="dob">
+          <FormattedMessage id="form.dob" />
+        </label>
+        <input
+          type="date"
+          {...register('dob', {
+            required: formatMessage({ id: 'form.required' }),
+          })}
+        />
+        <br />
+        {errors.dob?.message}
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
