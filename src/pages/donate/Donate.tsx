2452e9282ad144fcccc23c4d6bbe3eb53257a6e4
verden11 <justinas.reigis@gmail.com>
2022-04-01 21:13:40 +0300

    chore: add more content

--- src/pages/donate/Donate.tsx
+++ src/pages/donate/Donate.tsx
@@ -1,7 +1,41 @@
 import { memo } from 'react';
+import { FormattedMessage } from 'react-intl';
 
 const Donate = () => {
-  return <div>Donate</div>;
+  return (
+    <div className="max-w-6xl mx-6 md:mx-14 my-16">
+      <p className="text-3xl font-bold capitalize mb-16">
+        <FormattedMessage id="donate" />
+      </p>
+      <p>
+        <FormattedMessage id="donateSubText" />
+      </p>
+
+      <div className="text-base">
+        <p className="font-bold">VŠĮ “Stiprūs kartu”</p>
+        <p>Company code: 305674792</p>
+        <p>Address: P. Vileišio 18A, Vilnius</p>
+        <p>Bank account number: LT387044090101401806</p>
+        <p>AB SEB bankas</p>
+      </div>
+
+      <a href="mailto:mes@stipruskartu.lt" className="text-teal-800">
+        mes@stipruskartu.lt
+      </a>
+
+      <form target="_blank" action="https://www.paypal.com/donate" method="post" className="my-9">
+        <input type="hidden" name="hosted_button_id" value="Q9N2KYT5YLBP4" />
+        <div>Paypal</div>
+        <input
+          type="image"
+          src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+          name="submit"
+          title="Paremkite"
+          alt="Donate with PayPal button"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default memo(Donate);
--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -1,7 +1,36 @@
 import { memo } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+import { LocalRoute } from '../../common/constants/routes';
+import flag from '../../assets/ua_flag.svg';
+import savanoriaiLogo from '../../assets/savanoriai-logo.png';
+import arrowright from '../../assets/arrowright.svg';
 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <div className="max-w-6xl mx-6 md:mx-14 pt-3">
+      <section>
+        <h1 className="capitalize py-4 font-bold text-5xl text-center text-teal-800">
+          <FormattedMessage id="news" />
+        </h1>
+        <div className="flex flex-1 flex-wrap">
+          <div>
+            <img className="rounded" src={flag} width={130} height={90} alt="logo" />
+            <p className="max-w-xs pr-4">INFORMATION FOR UKRAINIAN NATIONALS AND THEIR FAMILY MEMBERS</p>
+          </div>
+
+          <div>
+            <img className="rounded" src={savanoriaiLogo} width={130} height={90} alt="logo" />
+            <p className="max-w-xs pr-4">Ačiū, #StiprūsKartu savanoriai!</p>
+          </div>
+        </div>
+        <Link to={LocalRoute.News} className="inline-flex text-teal-800">
+          <p className="capitalize mr-2">more</p>
+          <img className="rounded" src={arrowright} width={20} height={20} alt="logo" />
+        </Link>
+      </section>
+    </div>
+  );
 };
 
 export default memo(News);
