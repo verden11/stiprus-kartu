9c3071fc8fdf9f470dfa3f3d5356322bd0ea8f09
verden11 <justinas.reigis@gmail.com>
2022-04-01 21:21:30 +0300

    chore: some styles

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -9,26 +9,24 @@ import arrowright from '../../assets/arrowright.svg';
 const News = () => {
   return (
     <div className="max-w-6xl mx-6 md:mx-14 pt-3">
-      <section>
-        <h1 className="capitalize py-4 font-bold text-5xl text-center text-teal-800">
-          <FormattedMessage id="news" />
-        </h1>
-        <div className="flex flex-1 flex-wrap">
-          <div>
-            <img className="rounded" src={flag} width={130} height={90} alt="logo" />
-            <p className="max-w-xs pr-4">INFORMATION FOR UKRAINIAN NATIONALS AND THEIR FAMILY MEMBERS</p>
-          </div>
-
-          <div>
-            <img className="rounded" src={savanoriaiLogo} width={130} height={90} alt="logo" />
-            <p className="max-w-xs pr-4">Ačiū, #StiprūsKartu savanoriai!</p>
-          </div>
+      <h1 className="capitalize py-4 font-bold text-5xl text-center text-teal-800 mb-16">
+        <FormattedMessage id="news" />
+      </h1>
+      <div className="flex flex-1 flex-wrap">
+        <div className="flex flex-1 flex-col">
+          <img className="rounded" src={flag} width="50%" height={90} alt="logo" />
+          <p className="max-w-xs pr-4">INFORMATION FOR UKRAINIAN NATIONALS AND THEIR FAMILY MEMBERS</p>
         </div>
-        <Link to={LocalRoute.News} className="inline-flex text-teal-800">
-          <p className="capitalize mr-2">more</p>
-          <img className="rounded" src={arrowright} width={20} height={20} alt="logo" />
-        </Link>
-      </section>
+
+        <div className="flex flex-1 flex-col">
+          <img className="rounded" src={savanoriaiLogo} width="50%" height={90} alt="logo" />
+          <p className="max-w-xs pr-4">Ačiū, #StiprūsKartu savanoriai!</p>
+        </div>
+      </div>
+      <Link to={LocalRoute.News} className="inline-flex text-teal-800">
+        <p className="capitalize mr-2">more</p>
+        <img className="rounded" src={arrowright} width={20} height={20} alt="logo" />
+      </Link>
     </div>
   );
 };
