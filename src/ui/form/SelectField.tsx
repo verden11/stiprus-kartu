9116f8ab047c56c6d568bd56d5e7caf91c082a4e
Katanis <emiliskakanis@gmail.com>
2022-04-04 20:46:51 +0300

    feat: selectfield component created

---
+++ src/ui/form/SelectField.tsx
@@ -0,0 +1,74 @@
+import React, { ReactNode } from 'react';
+import { Box, FormControl, FormHelperText, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { Controller, ControllerProps, Path, PathValue, UnpackNestedValue } from 'react-hook-form';
+import { genericMemo } from './generick';
+
+interface SelectOption<T = string> {
+  label: ReactNode;
+  value: T;
+}
+
+interface Props<T> {
+  name: Path<T>;
+  control?: ControllerProps<T>['control'];
+  value?: UnpackNestedValue<PathValue<T, Path<T>>>;
+  defaultValue?: UnpackNestedValue<PathValue<T, Path<T>>>;
+  disabled?: boolean;
+  isRequired?: boolean;
+  label: string;
+  options: SelectOption[];
+  onChanged?(value: string): void;
+}
+
+const SelectFieldComponent = <T extends Record<string, any>>({
+  name,
+  control,
+  label,
+  options,
+  onChanged,
+  defaultValue,
+  disabled = false,
+  isRequired = false,
+}: Props<T>) => {
+  return (
+    <Box flex={1} display="flex" marginRight={1}>
+      <Controller<T>
+        name={name}
+        control={control}
+        defaultValue={defaultValue}
+        rules={{ required: isRequired }}
+        render={({ field, fieldState: { error } }) => (
+          <FormControl sx={{ width: '100%', flex: 1 }}>
+            <InputLabel classes={{ shrink: 'shrink' }} required={isRequired} error={!!error}>
+              {label}
+            </InputLabel>
+
+            <Select
+              {...field}
+              id={name}
+              label={label}
+              name={name}
+              error={!!error}
+              fullWidth
+              disabled={disabled}
+              required={isRequired}
+              onChange={field.onChange}
+              sx={{ minHeight: 36, whiteSpace: 'normal' }}
+            >
+              {options.map(({ label, value }) => (
+                <MenuItem key={value} value={value} sx={{ maxWidth: 300 }}>
+                  {label}
+                </MenuItem>
+              ))}
+            </Select>
+            <FormHelperText error={!!error}>{error?.message}</FormHelperText>
+          </FormControl>
+        )}
+      />
+    </Box>
+  );
+};
+
+export const SelectField = genericMemo(SelectFieldComponent);
+
+export const EMPTY_STR_OPTION: SelectOption = { label: '', value: '' };
