674c654a22174b1f83082b4e0f3193f11ec538f8
Justinas Reigis <verden11@users.noreply.github.com>
2022-04-04 20:09:13 +0300

    feat: page title translated (#9)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 import { Providers } from './Providers';
+import Helmet from './Helmet/Helmet';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
@@ -11,6 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Providers>
+      <Helmet />
       <App />
     </Providers>
   </React.StrictMode>,
