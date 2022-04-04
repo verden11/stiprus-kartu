0848a88734c36980f2ae3d35440998485a910681
Justinas Reigis <verden11@users.noreply.github.com>
2022-04-04 16:14:45 +0300

    feat: add translations (#8)
    * chore: filled out LT and UA translations
    
    * chore: added more translations and multilang labels
    
    * chore: more additions to multilang labels
    
    * chore: even more additions to multilang labels
    
    * chore: adding translations to Donate page
    
    Co-authored-by: ferame <alisauskas.j@gmail.com>

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import car from '../../assets/car.svg';
 import house from '../../assets/house.svg';
 import logo from '../../assets/stipruskartu_logo.svg';
@@ -12,11 +12,15 @@ import { Link } from 'react-router-dom';
 import { LocalRoute } from '../../common/constants/routes';
 
 const Home = () => {
+  const { formatMessage } = useIntl();
+  
   return (
     <div className="max-w-6xl mx-6 md:mx-14 pt-3">
       <section className="flex flex-1 flex-row-reverse md:flex-row justify-around items-center my-8">
         <img src={logo} width={64} height={64} alt="logo" />
-        <p className="flex-1 md:text-3xl font-bold md:mx-8">Assistance for war refugees from Ukraine</p>
+        <p className="flex-1 md:text-3xl font-bold md:mx-8">
+          <FormattedMessage id="home.title"/>
+        </p>
         <div className="flex-col hidden md:flex">
           <div className="flex flex-col text-sm">
             <a href="tel:1827" className="font-bold text-base">
@@ -35,36 +39,54 @@ const Home = () => {
 
       <section>
         <Card
-          title="Fleeing war in Ukraine?"
-          description="We'll find you a temporary home in Lithuania. Sign up, and our volunteers will take care of you."
-          buttons={[{ text: 'Get help', color: 'bg-amber-700', link: LocalRoute.FormHelp }]}
+          title={formatMessage({id:"home.card.refugee.title"})}
+          description={formatMessage({id:"home.card.refugee.description"})}
+          buttons={[{
+            text: formatMessage({id:"home.card.refugee.button"}),
+            color: 'bg-amber-700',
+            link: LocalRoute.FormHelp
+          }]}
         />
 
         <Card
-          title="Do you have a vacant flat, house, or room? Can you offer a ride?"
-          description="Sign up to help people fleeing war in Ukraine."
+          title={formatMessage({id:"home.card.volunteer.title"})}
+          description={formatMessage({id:"home.card.volunteer.description"})}
           buttons={[
-            { text: 'Offer accommodation', color: 'bg-teal-800', link: LocalRoute.FormAccommodation },
-            { text: 'Offer a ride from the border', color: 'bg-teal-800', link: LocalRoute.FormRide },
+            { 
+              text: formatMessage({id:"home.card.volunteer.button.accomodation"}),
+              color: 'bg-teal-800',
+              link: LocalRoute.FormAccommodation
+            },
+            { 
+              text: formatMessage({id:"home.card.volunteer.button.ride"}),
+              color: 'bg-teal-800',
+              link: LocalRoute.FormRide
+            },
           ]}
         />
       </section>
 
       <section className="my-14">
-        <p className="text-2xl font-normal mb-10">People in Lithuania have already offered:</p>
+        <p className="text-2xl font-normal mb-10">
+          <FormattedMessage id="home.statistics.title" />
+        </p>
         <div className="flex flex-1 flex-row justify-around">
           <div className="flex flex-1 flex-wrap justify-center items-center">
             <img width={54} height={54} src={house} alt="hosue" />
             <div className="md:ml-8 flex flex-col items-center">
               <p className="text-4xl font-bold">10119</p>
-              <p className="text-lg text-center">places to stay</p>
+              <p className="text-lg text-center">
+                <FormattedMessage id="home.statistics.placesToStay" />
+              </p>
             </div>
           </div>
           <div className="flex flex-1 flex-wrap justify-center items-center">
             <img width={54} height={54} src={car} alt="car" />
             <div className="md:ml-8 flex flex-col items-center">
               <p className="text-4xl font-bold">5009</p>
-              <p className="text-lg text-center">rides from the border</p>
+              <p className="text-lg text-center">
+                <FormattedMessage id="home.statistics.ridesFromBorder" />
+              </p>
             </div>
           </div>
         </div>
@@ -87,9 +109,15 @@ const Home = () => {
           className="flex flex-1 flex-col items-center text-white py-7 bg-no-repeat bg-center"
           style={{ backgroundImage: `url(${union})` }}
         >
-          <p className="text-3xl font-bold text-center">Ukraine needs you.</p>
-          <p className="text-3xl font-bold text-center mb-7">We're strong together!</p>
-          <p className="text-2xl font-normal text-center">National volunteer support coordination center</p>
+          <p className="text-3xl font-bold text-center">
+            <FormattedMessage id="home.banner.ukraineNeedsYou" />
+          </p>
+          <p className="text-3xl font-bold text-center mb-7">
+            <FormattedMessage id="home.banner.strongTogether"/>
+          </p>
+          <p className="text-2xl font-normal text-center">
+            <FormattedMessage id="home.banner.coordinationCenter" />
+          </p>
         </div>
       </section>
       <section>
@@ -125,7 +153,7 @@ const Home = () => {
       <section className="my-14">
         <div className="flex justify-center py-4">
           <p className="font-bold text-4xl max-w-sm text-center leading-10">
-            #StrongTogether is the initiative of the Pelėdos squad in the Lithuanian Riflemen's Union
+            <FormattedMessage id="home.hashtag.strongTogether" />
           </p>
         </div>
       </section>
